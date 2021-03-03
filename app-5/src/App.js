948a6abf42fbcd284498c7ffbbe87fdc5c704dc3
djkoser <koser.david@gmail.com>
2021-03-03 17:38:02 -0600

    Reinitialized create-react-app on app-5 unable to solve previous error

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,25 +1,25 @@
-import React, {Component} from 'react';
+import logo from './logo.svg';
 import './App.css';
-// import Image from './Components/Image'
 
-class App extends Component() {
-  constructor() {
-    super(); 
-    this.state = {
-      imageURL: "https://via.placeholder.com/150/FFFF00/000000?Text=David's Website"
-    };
-  };
-
-  render () {
-    console.log(this);
-    return (
+function App() {
+  return (
     <div className="App">
       <header className="App-header">
-        {/* <Image pic={this.state.imageURL}/> */}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
-    )
-  };
-};
+  );
+}
 
-export default App
+export default App;
